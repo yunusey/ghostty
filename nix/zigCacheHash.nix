622cc3f9c7c0181e1f57c09b88f5ca53f1499b98
Mitchell Hashimoto <m@mitchellh.com>
2025-01-08 19:51:16 -0800

    build: update zig-wayland to use new LazyPath API
    This is a more idiomatic way to handle build paths in Zig 0.13 and
    later.

--- nix/zigCacheHash.nix
+++ nix/zigCacheHash.nix
@@ -1,3 +1,3 @@
 # This file is auto-generated! check build-support/check-zig-cache-hash.sh for
 # more details.
-"sha256-PnfSy793kcVt85q47kWR0xkivXoMOZAAmuUyKO9vqAI="
+"sha256-MeSJiiSDDWZ7vUgY56t9aPSLPTgIKb4jexoHmDhJOGM="
