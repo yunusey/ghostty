af5e423ea5086b1acb6b265db0870ac79ba5e405
Bryan Lee <38807139+liby@users.noreply.github.com>
2025-01-12 01:48:48 +0800

    Clear selection when IME input starts

--- macos/Sources/Ghostty/SurfaceView_AppKit.swift
+++ macos/Sources/Ghostty/SurfaceView_AppKit.swift
@@ -1293,6 +1293,11 @@ extension Ghostty.SurfaceView: NSTextInputClient {
     }
 
     func setMarkedText(_ string: Any, selectedRange: NSRange, replacementRange: NSRange) {
+        // Clear selection when IME input starts
+        if let surface = self.surface, ghostty_surface_has_selection(surface) {
+            ghostty_surface_clear_selection(surface)
+        }
+
         switch string {
         case let v as NSAttributedString:
             self.markedText = NSMutableAttributedString(attributedString: v)
