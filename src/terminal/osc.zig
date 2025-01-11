95fc1d64c89f90aa72cc45aa6617b50121f1a4e5
Damien Mehala <damien.mehala@gmail.com>
2025-01-11 17:24:13 +0100

    parse ConEmu OSC9;5

--- src/terminal/osc.zig
+++ src/terminal/osc.zig
@@ -178,6 +178,9 @@ pub const Command = union(enum) {
         progress: ?u8 = null,
     },
 
+    /// Wait input (OSC 9;5)
+    wait_input: void,
+
     pub const ColorKind = union(enum) {
         palette: u8,
         foreground,
@@ -377,6 +380,7 @@ pub const Parser = struct {
         conemu_progress_state,
         conemu_progress_prevalue,
         conemu_progress_value,
+        conemu_wait,
     };
 
     /// This must be called to clean up any allocated memory.
@@ -811,6 +815,11 @@ pub const Parser = struct {
                 '4' => {
                     self.state = .conemu_progress_prestate;
                 },
+                '5' => {
+                    self.state = .conemu_wait;
+                    self.command = .{ .wait_input = {} };
+                    self.complete = true;
+                },
 
                 // Todo: parse out other ConEmu operating system commands.
                 // Even if we don't support them we probably don't want
@@ -941,6 +950,11 @@ pub const Parser = struct {
                     self.state = .swallow;
                     self.complete = true;
                 },
+            },
+
+            .conemu_wait => {
+                self.state = .invalid;
+                self.complete = false;
             },
 
             .query_fg_color => switch (c) {
@@ -2094,6 +2108,30 @@ test "OSC: OSC9 progress pause with progress" {
     try testing.expect(cmd == .progress);
     try testing.expect(cmd.progress.state == .pause);
     try testing.expect(cmd.progress.progress == 100);
+}
+
+test "OSC: OSC9 conemu wait input" {
+    const testing = std.testing;
+
+    var p: Parser = .{};
+
+    const input = "9;5";
+    for (input) |ch| p.next(ch);
+
+    const cmd = p.end('\x1b').?;
+    try testing.expect(cmd == .wait_input);
+}
+
+test "OSC: OSC9 conemu wait invalid input" {
+    const testing = std.testing;
+
+    var p: Parser = .{};
+
+    const input = "9;5;";
+    for (input) |ch| p.next(ch);
+
+    const cmd = p.end('\x1b');
+    try testing.expect(cmd == null);
 }
 
 test "OSC: empty param" {
