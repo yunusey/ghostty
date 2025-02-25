a773588c991d8a18348575c44e8e1d166de491ba
Leah Amelia Chen <hi@pluie.me>
2025-02-25 08:55:56 +0100

    gtk: remove c.zig
    It has been done.

--- src/apprt/gtk/c.zig
+++
@@ -1,8 +0,0 @@
-/// Imported C API directly from header files
-pub const c = @cImport({
-    @cInclude("gtk/gtk.h");
-    @cInclude("adwaita.h");
-
-    // compatibility
-    @cInclude("ghostty_gtk_compat.h");
-});
