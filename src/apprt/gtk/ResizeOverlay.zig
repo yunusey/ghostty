0e6c26bbfed93df09e2d5a33c310773a65c4c1fc
Tristan Partin <tristan@partin.io>
2025-03-01 00:37:01 -0600

    apprt/gtk: remove non-ascii characters from resize overlay
    It is possible that fonts people are using don't contain these
    characters as evidenced by users in the Discord.
    
    Signed-off-by: Tristan Partin <tristan@partin.io>

--- src/apprt/gtk/ResizeOverlay.zig
+++ src/apprt/gtk/ResizeOverlay.zig
@@ -114,7 +114,7 @@ fn gtkUpdate(ud: ?*anyopaque) callconv(.C) c_int {
     var buf: [32]u8 = undefined;
     const text = std.fmt.bufPrintZ(
         &buf,
-        "{d} ↔ ⨯ {d} ↕",
+        "{d} x {d}",
         .{
             grid_size.columns,
             grid_size.rows,
