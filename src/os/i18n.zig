52ac670913b3339256317d349564ed93762f95b2
Mitchell Hashimoto <m@mitchellh.com>
2025-04-11 09:47:09 -0700

    CODEOWNERS

--- src/os/i18n.zig
+++ src/os/i18n.zig
@@ -23,6 +23,12 @@ const log = std.log.scoped(.i18n);
 ///
 ///   3. Most preferred locale for a language without a country code.
 ///
+/// Note for "most common" locales, this is subjective and based on
+/// the perceived userbase of Ghostty, which may not be representative
+/// of general populations or global language distribution. Also note
+/// that ordering may be weird when we first merge a new locale since
+/// we don't have a good way to determine this. We can always reorder
+/// with some data.
 pub const locales = [_][:0]const u8{
     "zh_CN.UTF-8",
     "de_DE.UTF-8",
