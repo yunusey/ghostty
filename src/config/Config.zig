6c2ea8637e8201068033b84ed4b37627d01ae858
Mitchell Hashimoto <m@mitchellh.com>
2025-06-25 16:27:12 -0400

    config: add more docs for `background-image`

--- src/config/Config.zig
+++ src/config/Config.zig
@@ -468,8 +468,17 @@ foreground: Color = .{ .r = 0xFF, .g = 0xFF, .b = 0xFF },
 
 /// Background image for the terminal.
 ///
-/// This should be a path to a PNG or JPEG file,
-/// other image formats are not yet supported.
+/// This should be a path to a PNG or JPEG file, other image formats are
+/// not yet supported.
+///
+/// The background image is currently per-terminal, not per-window. If
+/// you are a heavy split user, the background image will be repeated across
+/// splits. A future improvement to Ghostty will address this.
+///
+/// WARNING: Background images are currently duplicated in VRAM per-terminal.
+/// For sufficiently large images, this could lead to a large increase in
+/// memory usage (specifically VRAM usage). A future Ghostty improvement
+/// will resolve this by sharing image textures across terminals.
 @"background-image": ?Path = null,
 
 /// Background image opacity.
