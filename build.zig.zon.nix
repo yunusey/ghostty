d0e12cc0821f188820675548d04c1fec1b52a93c
Mitchell Hashimoto <m@mitchellh.com>
2025-06-29 15:06:18 -0700

    update libxev to workaround the io_uring regression in Linux 6.15.4
    Fixes #7724
    
    Background at the end of the commit message. The fix in libxev is
    described in the PR and commit we pin to here, but basically we swap
    read for poll for eventfd/timerfd.
    
    From Jens Axboe on X:
    
    > This will fix it: https://pastebin.com/n7JSZWpW which makes me suspicious
    > that it's an S_IFREG check somewhere else, as anon inodes are now listed as
    > regular files. Has potentially pretty broad implications...
    
    > I think I can already answer why that breaks things - io_uring checks if
    > this is a regular file, and if it is, it doesn't do short reads. Short
    > reads on regular files (or a bdev) will cause application issues, as
    > basically nobody expects them.
    
    > Now we have what acts like a char dev, but where io_uring will retry IO
    > because the application asked for more data than what was delivered. This
    > will cause the weird slowdowns as data isn't delivered as soon as it's
    > available.

--- build.zig.zon.nix
+++ build.zig.zon.nix
@@ -186,11 +186,11 @@ in
       };
     }
     {
-      name = "libxev-0.0.0-86vtc5b1EgB7vFmt9Tk7ySteR5AeEHW7xcR6gK9dMUD3";
+      name = "libxev-0.0.0-86vtcyMBEwA-UpcjfOICyI2GYG8o6MiRbinS1_8g1_rw";
       path = fetchZigArtifact {
         name = "libxev";
-        url = "https://github.com/mitchellh/libxev/archive/9bc52324d4f0c036a3b244e992680a9fb217bbd3.tar.gz";
-        hash = "sha256-VwFByDoptqiN5UkolFQ7TbRhwMERReD9Er2pjxTCYIU=";
+        url = "https://github.com/mitchellh/libxev/archive/75a10d0fb374e8eb84948dcfc68d865e755e59c2.tar.gz";
+        hash = "sha256-/CSKSuZZfn0aIQlVZ0O8ch5O4gCajYBTTuoetRdo0n4=";
       };
     }
     {
